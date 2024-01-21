3673d2cacff3f271bc3ee506a17614b5847de683
Sahilgupta942 <61141032+Sahilgupta942@users.noreply.github.com>
2024-01-21 13:10:10 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,20 @@
+module "security_group" {
+  source = "./securitygroup_module"
+}
+
+module "S3" {
+  source = "./s3_module"
+}
+
+module "EC2" {
+  source = "./ec2_module"
+}
+
+resource "aws_instance" "Terraform-Sahilkgup-instance" {
+  ami           = "ami-0dab0800aa38826f2"
+  instance_type = "t2.micro"
+  security_groups = [module.security_group.SecurityGroup_terraform]
+  tags = {
+    Name = "Terraform_main_test"
+  }
+}
