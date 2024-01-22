dccc1e683910d03494a59eb2cd2dcf3752684d3c
Sahilgupta942 <61141032+Sahilgupta942@users.noreply.github.com>
2024-01-22 16:34:18 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -11,10 +11,19 @@ module "EC2" {
 }
 
 resource "aws_instance" "Terraform-Sahilkgup-instance" {
-  ami           = "ami-0dab0800aa38826f2"
+  ami           = lookup(var.AMIs, var.AWS_REGION)
   instance_type = "t2.micro"
   security_groups = [module.security_group.SecurityGroup_terraform]
   tags = {
     Name = "Terraform_main_test"
+  }
+}
+
+resource "aws_instance" "Terraform-Sahilkgup-instance" {
+  ami           = "ami-0dab0800aa38826f2"
+  instance_type = "t2.micro"
+  security_groups = var.Security_Groups
+  tags = {
+    Name = "Terraform_main_test_dummy"
   }
 }
